c6bb9c0e875265174844cb5e6a2094d342325f47
collins <collinsolayemi@gmail.com>
2023-01-19 05:52:25 -0800

    code functionality

--- controller/user.js
+++ controller/user.js
@@ -1,4 +1,7 @@
 const User = require("../model/user");
+const statusCode = require("http-status-codes");
+const { StatusCodes } = require("http-status-codes/build/cjs/status-codes");
+const cryptoJS = require("crypto-js");
 
 exports.getUser = (req, res) => {
   res.send("get user");
@@ -7,13 +10,16 @@ exports.getUser = (req, res) => {
 exports.createUser = async (req, res) => {
   const newUser = new User({
     username: req.body.username,
-    password: req.body.password,
+    password: cryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASSWORD_SECRET
+    ),
     email: req.body.email,
   });
 
   try {
     const savedUser = await newUser.save();
-    res.status(201).json({ savedUser });
+    res.status(StatusCodes.OK).json({ savedUser });
   } catch (err) {
     console.log(err);
   }
--- routes/auth.js
+++ routes/auth.js
@@ -1,21 +1,5 @@
 const router = require("express").Router();
 const userControl = require("../controller/user");
-// const User = require("../model/user");
-
-// router.post("/regiter", async (req, res) => {
-//   const newUser = new User({
-//     username: req.body.username,
-//     password: req.body.password,
-//     email: req.body.email,
-//   });
-
-//   try {
-//     const savedUser = await newUser.save();
-//     res.status(201).json({ savedUser });
-//   } catch (err) {
-//     console.log(err);
-//   }
-// });
 
 router.route("/register").get(userControl.getUser).post(userControl.createUser);
 
