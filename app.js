69dfe7b64fc5da3e0e3e30a8d963c353baa1bc84
collins <collinsolayemi@gmail.com>
2023-01-19 00:49:43 -0800

    refacturing spme codes

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
 
-const port = 1000;
+const DB = process.env.DATABASE_URL;
+mongoose
+  .set("strictQuery", false)
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB connected"));
+
+const port = process.env.PORT || 1000;
+
 app.listen(port, () => {
-  console.log(`App running on port ${port}`);
+  console.log(`backnd running on port ${port}`);
 });
-
-const start = () => {
-  const port = process.env.PORT || 1000;
-  console.log(`APP running on port ${port}`);
-};
-
-start();
