45094004aa8636ccc7c98451e0f61fcca1cb03b4
collins <collinsolayemi@gmail.com>
2023-01-19 16:41:57 -0800

    implementing the routes

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,28 @@
 const router = require("express").Router();
-const verifyToken = require("../JWT/verify-token");
+const { verifyToken, verifyAndAuthorization } = require("../JWT/verify-token");
+const cryptoJS = require("crypto-js");
+const { StatusCodes } = require("http-status-codes/build/cjs/status-codes");
+const User = require("../model/user");
 
-router.route("/:id").put();
+router.put("/:id", verifyAndAuthorization, async (req, res) => {
+  if (req.body.password) {
+    req.body.password = cryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASSWORD_SECRET
+    ).toString();
+  }
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(StatusCodes.OK).json(updatedUser);
+  } catch (err) {
+    res.status(StatusCodes.NOT_FOUND).json();
+  }
+});
 
 module.exports = router;
