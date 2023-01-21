9afcb4916b216aa537a538a78f2f28498950a144
collins <collinsolayemi@gmail.com>
2023-01-21 15:14:03 -0800

    adding the controllers functionality

---
+++ controller/user.js
@@ -0,0 +1,57 @@
+const { StatusCodes } = require("http-status-codes/build/cjs/status-codes");
+const User = require("../model/user");
+const cryptoJS = require("crypto-js");
+
+//get all users
+exports.getAllUsers = async (req, res) => {
+  try {
+    const getAllUser = await User.find();
+    console.log(getAllUser);
+    return res.status(StatusCodes.OK).json(getAllUser);
+  } catch (err) {
+    res.status(StatusCodes.NOT_FOUND).json(err);
+  }
+};
+
+//UPDATE USER
+exports.updateUser = async (req, res) => {
+  if (req.body.password) {
+    req.body.password = cryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASSWORD_SECRET
+    ).toString();
+  }
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(StatusCodes.OK).json(updatedUser);
+  } catch (err) {
+    res.status(StatusCodes.NOT_FOUND).json();
+  }
+};
+
+//DELETE USER
+exports.deleteUser = async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(StatusCodes.ACCEPTED).json("deleted successfully!");
+  } catch (err) {
+    res.status(StatusCodes.NOT_FOUND).json(err);
+  }
+};
+
+//GET ONE USER
+exports.getuser = async (req, res) => {
+  try {
+    const getUser = await User.findById(req.params.id);
+    const { password, ...user } = getUser._doc;
+    return res.status(StatusCodes.OK).json(user);
+  } catch (err) {
+    res.status(StatusCodes.NOT_FOUND).json(err);
+  }
+};
--- routes/user.js
+++ routes/user.js
@@ -4,63 +4,15 @@ const {
   verufyAndAuthorize,
   verifyTokenAndAdmin,
 } = require("../JWT/verify-token");
-const cryptoJS = require("crypto-js");
-const { StatusCodes } = require("http-status-codes/build/cjs/status-codes");
-const User = require("../model/user");
+const userController = require("../controller/user");
 
-//update the user
-router.put("/:id", verufyAndAuthorize, async (req, res) => {
-  if (req.body.password) {
-    req.body.password = cryptoJS.AES.encrypt(
-      req.body.password,
-      process.env.PASSWORD_SECRET
-    ).toString();
-  }
-  try {
-    const updatedUser = await User.findByIdAndUpdate(
-      req.params.id,
-      {
-        $set: req.body,
-      },
-      { new: true }
-    );
-    res.status(StatusCodes.OK).json(updatedUser);
-  } catch (err) {
-    res.status(StatusCodes.NOT_FOUND).json();
-  }
-});
+router.route("/find/:id").get(userController.getuser);
+router.route("/").get(userController.getAllUsers);
+router
+  .route("/:id")
+  .put(verufyAndAuthorize, userController.updateUser)
+  .delete(userController.deleteUser);
 
-//DELETE THE USER
-router.delete("/:id", verufyAndAuthorize, async (req, res) => {
-  try {
-    await User.findByIdAndDelete(req.params.id);
-    res.status(StatusCodes.ACCEPTED).json("deleted successfully!");
-  } catch (err) {
-    res.status(StatusCodes.NOT_FOUND).json(err);
-  }
-});
-
-//GET THE USER
-router.get("/find/:id", async (req, res) => {
-  try {
-    const getUser = await User.findById(req.params.id);
-    const { password, ...user } = getUser._doc;
-    return res.status(StatusCodes.OK).json(user);
-  } catch (err) {
-    res.status(StatusCodes.NOT_FOUND).json(err);
-  }
-});
-
-//GET ALL USERS
-router.get("/", async (req, res) => {
-  try {
-    const getAllUser = await User.find();
-    const { password, ...others } = getAllUser;
-    console.log(getAllUser, password);
-    return res.status(StatusCodes.OK).json(getAllUser);
-  } catch (err) {
-    res.status(StatusCodes.NOT_FOUND).json(err);
-  }
-});
+//ADDING STats
 
 module.exports = router;
