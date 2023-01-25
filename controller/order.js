17749e59e46eb5b644e6980344f6e83b4cf18afa
collins <collinsolayemi@gmail.com>
2023-01-25 15:48:54 -0800

    order controller

--- controller/order.js
+++ controller/order.js
@@ -64,6 +64,22 @@ exports.getStats = async (req, res) => {
   const lastMonth = new Date(date.setMonth(date.getMonth() - 1));
   const previousMonth = new Date(new Date().setMonth(lastMonth.getMonth() - 1));
   try {
+    const income = await Order.aggregate([
+      { $match: { createdAt: { $gte: previousMonth } } },
+      {
+        $project: {
+          month: { $month: $createdAt },
+          sales: "$amount",
+        },
+      },
+      {
+        $group: {
+          _id: "$month",
+          total: { $sum: "$sales" },
+        },
+      },
+    ]);
+    res.status(StatusCodes.OK).json(income);
   } catch (err) {
     res.status(StatusCodes.BAD_REQUEST).json(err);
   }
